dbdf0090dbfef9996e05107d942e2f4f6fbb8e0c
Jeremy Hettenhouser <jeremy.hettenhouser@icf.com>
2025-04-14 10:59:51 -0400

    feat: Initial Template Checkin

---
+++ projects/db/src/lib/db.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-db',
+  imports: [],
+  template: `
+    <p>
+      db works!
+    </p>
+  `,
+  styles: ``
+})
+export class DbComponent {
+
+}
---
+++ projects/db/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of db
+ */
+
+export * from './lib/db.service';
+export * from './lib/db.component';
